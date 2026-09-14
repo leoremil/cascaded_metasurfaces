33d6b318dc74a3e57bd07d5adbe73c54f740eade
leoremil <lremilla@ualberta.ca>
2026-09-14 17:13:03 -0600

    Began derivations

--- transformation_matrix_derivation.py
+++ transformation_matrix_derivation.py
@@ -5,7 +5,7 @@ Created on Mon Sep 14 16:46:23 2026
 @author: lremilla
 """
 
-from sympy import symbols, Function, Eq, I
+from sympy import symbols, Function, Eq, I, exp
 from sympy.vector import CoordSys3D
 
 #Create cylindrical coordinate system
@@ -14,12 +14,19 @@ coordinate_system = CoordSys3D('CS', transformation='cylindrical',variable_names
 #Define the symbols needed
 
 E_til, H_til = symbols(r"\tilde{E} \tilde{H}", cls=Function)
+E, H = symbols("E H", cls=Function)
 m, eps, mu = symbols("m epsilon mu")
 rho_n, rho_PEC = symbols("rho_n rho_PEC")
 k_z = symbols("k_z")
 t, w = symbols("t omega")
 
+rho = coordinate_system.rho
+phi = coordinate_system.phi
+z = coordinate_system.z
+
 E_rho, E_phi, E_z = symbols("E_rho E_phi E_z", cls=Function)
 H_rho, H_phi, H_z = symbols("H_rho H_phi H_z", cls=Function)
 
-E_full = Eq(E_til(rho,phi,z,t),)
+E_full = Eq(E_til(rho,phi,z,t),E(rho,m,k_z)*exp(-I*m*phi)*exp(-I*k_z*z)*exp(I*w*t))
+H_full = Eq(H_til(rho,phi,z,t),H(rho,m,k_z)*exp(-I*m*phi)*exp(-I*k_z*z)*exp(I*w*t))
+
